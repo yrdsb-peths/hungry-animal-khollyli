67bb3385684c8477e492fc65129f41de74fbfa58
khollyli <94080024+khollyli@users.noreply.github.com>
2024-09-19 12:57:21 -0400

    Set random x location for Apple

--- Elephant.java
+++ Elephant.java
@@ -23,5 +23,8 @@ public class Elephant extends Actor
         {
             move(1);
         }
+        
+        // Remove apple if elephant eats it
+        removeTouching(Apple.class);
     }
 }
