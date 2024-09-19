20ab9dcfac220f12a22153f23f95e27037d5da9d
khollyli <94080024+khollyli@users.noreply.github.com>
2024-09-19 12:49:38 -0400

    Animate Elephant and Apple

--- Apple.java
+++ Apple.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Apple here.
+ * Food for our elephant.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Kholly Li
+ * @version September 2024
  */
 public class Apple extends Actor
 {
@@ -15,5 +15,6 @@ public class Apple extends Actor
     public void act()
     {
         // Add your action code here.
+        setLocation(getX(), getY() + 1);
     }
 }
