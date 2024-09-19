de2a0d9a9817eb1fb4cce08528dcf42a3ab41359
khollyli <94080024+khollyli@users.noreply.github.com>
2024-09-19 13:17:53 -0400

    Create gameOver label

--- MyWorld.java
+++ MyWorld.java
@@ -18,18 +18,27 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(600, 400, 1, false);
         
         // Create the elephant object
         Elephant elephant = new Elephant();
-        addObject(elephant, 300, 200);
+        addObject(elephant, 300, 300);
         
         // Create a label
         scoreLabel = new Label(0, 80);
         addObject(scoreLabel, 50, 50);
         
         
-        createApple();
+        spawnApple();
+    }
+    
+    /**
+     * End the game and draw 'GameOver'
+     */
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
     }
     
     /**
@@ -44,7 +53,7 @@ public class MyWorld extends World
     /**
      * Create a new apple at random location at top of screen
      */
-    public void createApple()
+    public void spawnApple()
     {
         Apple apple = new Apple();
         int x = Greenfoot.getRandomNumber(600);
