489c01dd0633e9579875e3f5ff61f4b4c645505f
khollyli <94080024+khollyli@users.noreply.github.com>
2024-09-23 13:03:27 -0400

    Add elephant sound
    When elephant and apple actor touches, it plays an elephant sound

--- Elephant.java
+++ Elephant.java
@@ -8,10 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
-    /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    
     public void act()
     {
         // Add your action code here.
@@ -39,6 +37,7 @@ public class Elephant extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.spawnApple();
             world.increaseScore();
+            elephantSound.play();
         }
     }
 }
